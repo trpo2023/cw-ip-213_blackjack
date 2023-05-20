ceca7b1ba501cf21e96acfb4bbb1ebe0e7530c6a
Klonin <dimalmmm@mail.ru>
2023-05-20 18:45:27 +0700

    feat: added basic runer

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"course/internal/blackjack"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	bj := blackjack.NewBlackjack(blackjack.Config{})
+	err := bj.Run()
+
+	if err != nil {
+		fmt.Println("Error when running blackjack game:", err)
+	}
 }
--- internal/blackjack/blackjack.go
+++ internal/blackjack/blackjack.go
@@ -3,3 +3,25 @@
 */
 
 package blackjack
+
+import "fmt"
+
+type Blackjack struct{}
+
+type Config struct{}
+
+func NewBlackjack(cfg Config) *Blackjack {
+	return &Blackjack{}
+}
+
+func (b *Blackjack) printWelcome() {
+	fmt.Println(" ___   ___   ___   ___   ___ \n |A  | |K  | |Q  | |J  | |10 |\n |(`)| |(`)| |(`)| |(`)| |(`)|\n |_\\_| |_\\_| |_\\_| |_\\_| |_\\_|")
+	fmt.Println("--- Welcome in the Blackjack Game ---")
+	fmt.Println("-------------------------------------")
+}
+
+func (b *Blackjack) Run() error {
+	b.printWelcome()
+
+	return nil
+}
