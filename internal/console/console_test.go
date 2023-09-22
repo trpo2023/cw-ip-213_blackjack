e80fb2927a72762842e1d9571c7d2019b10a853b
klonin <dimalmmm@mail.ru>
2023-09-22 21:21:33 +0700

    test: added tests for console

--- internal/console/console_test.go
+++ internal/console/console_test.go
@@ -1,19 +1,87 @@
 package console
 
 import (
+	"bufio"
 	"github.com/stretchr/testify/require"
+	"os"
+	"sync"
 	"testing"
+	"time"
 )
 
-func newConsole(t *testing.T) *Console {
-	cnsl, err := NewConsole()
+func TestNewConsole(t *testing.T) {
+	testCases := []struct {
+		name  string
+		check func(c *Console, err error)
+	}{
+		{
+			name: "Ok",
+			check: func(c *Console, err error) {
+				require.NoError(t, err)
+				require.NotNil(t, c)
+			},
+		},
+	}
 
-	require.NoError(t, err)
-	require.Empty(t, cnsl)
+	for i := range testCases {
+		tc := testCases[i]
 
-	return cnsl
+		t.Run(tc.name, func(t *testing.T) {
+			c, err := NewConsole()
+			tc.check(c, err)
+		})
+	}
 }
 
-func TestNewConsole(t *testing.T) {
-	newConsole(t)
+func TestConsole_Input(t *testing.T) {
+	testCases := []struct {
+		name  string
+		check func(c *Console)
+	}{
+		{
+			name: "Ok",
+			check: func(c *Console) {
+				timer := time.AfterFunc(time.Second, func() {
+					t.Fatal("timeout")
+				})
+				defer timer.Stop()
+
+				var wg sync.WaitGroup
+
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					c.Input()
+				}()
+
+				writer := bufio.NewWriter(os.Stdout)
+				_, err := writer.WriteString("a")
+				require.NoError(t, err)
+
+				wg.Wait()
+			},
+		},
+		{
+			name: "Timeout",
+			check: func(c *Console) {
+				timer := time.AfterFunc(time.Second, func() {
+					t.Log("timeout")
+				})
+				defer timer.Stop()
+
+				c.Input()
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			c, err := NewConsole()
+			require.NoError(t, err)
+
+			tc.check(c)
+		})
+	}
 }
